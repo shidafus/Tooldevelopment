84862f378e958ba3a8e08b71fae76dab7a6e3cbc
zjh <zjh13320020268@163.com>
2023-07-31 17:38:56 +0800

    新增shell脚本

--- shell/mysql/mysql_dump_script.sh
+++ shell/mysql/mysql_dump_script.sh
@@ -2,6 +2,7 @@
 #################################################################################################################
 ##   create_time    : 2023/6/30                                                                                ##
 ##   Author         : Ops-DaBao                                                                                ##
+##   Mail            : zjh13320020268@163.com                                                                  ##
 ##   Feature        : Backing up mysql Data                                                                    ##
 ##   Usage          : When executing the script, you can pass parameters.                                      ##
 ##                    If you do not pass parameters, the default is used                                       ##
@@ -10,30 +11,65 @@
 red_color="\033[31m"
 green_color="\033[32m"
 text_end="\033[0m"
-
-#保存备份个数，备份31天数据
-number=31
-#日期
+numberdays=31
+start_time=$(date +%s)
 dd=`date +%Y-%m-%d-%H-%M-%S`
-#备份保存路径
-backup_dir=/mysqlbackup/$dd
-#备份工具
-tool=mysqldump
-#用户名
-username="root"
-#密码
-password="root"
-#将要备份的数据库
-database_name=mydatabase
+backup_dir="/data/backup/mysql"
+username=""
+password=""
+database_name=""
+
+
+backup_single_database() {
+	# 开始备份数据，单个数据库独立备份
+	echo -e "${red_color}----------本次进行全量单库for循环备份-----------$text_end"
+	for DB in $(docker exec  MySQL8 /bin/bash -c  "mysql -u $username -p$password  -e 'show databases' -s --skip-column-names"); do
+	    docker exec MySQL8 /bin/bash -c "mysqldump -u $username -p$password --single-transaction  --set-gtid-purged=off --hex-blob --events --routines $DB" 1> $backup_dir/$DB-$dd.sql 2> /tmp/$DB-$dd.log;
+	    echo -e "${green_color}-----------${DB}数据库备份完毕-----------$text_end"
+	done
+	echo -e "${green_color}-----------全部备份完毕！-----------$text_end"
+	echo -e "${green_color}-----------数据保存在 （$backup_dir/$DB-$dd.sql） 中-----------$text_end"
+}
+
+full_backup() {
+	# 备份所有数据到同一份文件中
+	echo -e "${red_color}-----------本次进行全量备份-----------$text_end"
+	docker exec MySQL* /bin/bash -c "mysqldump -u $username -p$password --single-transaction --set-gtid-purged=off --hex-blob --events --routines $database_name" 1> $backup_dir/$database_name-$dd.sql 2> /tmp/$database_name-$dd.log
+	echo -e "${green_color}----------备份完毕！-----------$text_end"
+	echo -e "${green_color}-----------数据保存在 （$backup_dir/$database_name-$dd.sql） 中-----------$text_end"
+}
+
+spatial_optimization() {
+	#找出需要删除的备份
+	echo -e "${red_color}----------开始查询过期数据-----------$text_end"
+	find /mysqlbackup/* -maxdepth 1 -type d -ctime +${numberdays} -exec rm -rf {} \;
+	echo -e "${green_color}-----------过期数据清理完毕！-----------$text_end"
+}
+
+
+usage() {
+  echo "脚本使用方法: bash $0 <选项> <参数>"
+  echo "选项说明:"
+  echo " -f <目录路径>: 数据保存路径 （默认为 /data/backup/mongodb）"
+  echo " -u <用户名>: 指定用户名 (默认为空)"
+  echo " -p <密码>: 指定密码 (默认为空)"
+  echo " -d <数据库名>: 指定数据库名 (默认为空)"
+  echo " -t <备份方式>: single  or  all (默认为all)  当为all时，指定的数据库参数无效"
+  echo " -n <天数>: 指定删除多少天前的无效数据 (默认为31天)"
+  exit 1
+}
 
 
 # 检查是否有参数传递，如果有则覆盖默认值
-while getopts "d:u:p:*" opt; do
+while getopts ":f:u:p:d:t:*:" opt; do
   case $opt in
-    d) backup_dir="$OPTARG";;
+    f) backup_dir="$OPTARG";;
     u) username="$OPTARG";;
     p) password="$OPTARG";;
-    *) echo "未知参数";;
+    d) database_name="$OPTARG";;
+    t) tip="$OPTARG";;
+    n) numberdays="$OPTARG";;
+    *) echo "参数传入有误: -$OPTARG"; usage;;
   esac
 done
 echo "Backup directory: $backup_dir"
@@ -44,30 +80,22 @@ if [ ! -d $backup_dir ]; then
     mkdir -p $backup_dir;
 fi
 
-backup_single_database() {
-	# 开始备份数据，单个数据库独立备份
-	echo -e "${red_color}----------本次进行全量单库for循环备份-----------$text_end"
-	for DB in $(docker exec  MySQL8 /bin/bash -c  "mysql -u $username -p$password  -e 'show databases' -s --skip-column-names"); do
-	    docker exec MySQL8 /bin/bash -c "$tool -u $username -p$password --single-transaction  --set-gtid-purged=off --hex-blob --events --routines $DB" 1> $backup_dir/$dd-$DB.sql 2> /tmp/$dd-$DB.log;
-	    echo -e "${green_color}-----------${DB}数据库备份完毕-----------$text_end"
-	done
-	echo -e "${green_color}-----------全部备份完毕！-----------$text_end"
-}
+case $tip in
+    all)
+      echo -e "${green_color}Mysql database_name: ${database_name}${text_end}"
+      full_backup;
+      spatial_optimization;
+      ;;
+    single)
+      echo -e "${green_color}Mysql database_name: ${database_name}${text_end}"
+      backup_single_database;
+      spatial_optimization;
+      ;;
+    *)
+      echo -e "${red_color}暂时无法确认你要备份那些数据${text_end}"
+esac
 
-full_backup() {
-	# 备份所有数据到同一份文件中
-	echo -e "${red_color}-----------本次进行全量备份-----------$text_end"
-	docker exec MySQL* /bin/bash -c "$tool -u $username -p$password --single-transaction --set-gtid-purged=off --hex-blob --events --routines $database_name" 1> $backup_dir/$dd-$database_name.sql 2> /tmp/$dd-$database_name.log
-	echo -e "${green_color}----------备份完毕！-----------$text_end"
-}
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
 
-spatial_optimization() {
-	#找出需要删除的备份
-	echo -e "${red_color}----------开始查询过期数据-----------$text_end"
-	find /mysqlbackup/* -maxdepth 1 -type d -ctime +31 -exec rm -rf {} \;
-	echo -e "${green_color}-----------过期数据清理完毕！-----------$text_end"
-}
-
-
-backup_single_database;
-spatial_optimization;
+echo -e "${green_color}本次执行脚本耗时为：$execution_time 秒${text_end}"
--- shell/mysql/mysql_resocer_script.sh
+++ shell/mysql/mysql_resocer_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 ##################################################################################################################
 ##   Create_time     : 2023/6/30                                                                                ##
 ##   Author          : Ops-DaBao                                                                                ##
+##   Mail            : zjh13320020268@163.com                                                                   ##
 ##   Feature         : restore mysql Data                                                                       ##
 ##   Usage           : When executing the script, you can pass parameters.                                      ##
 ##                     If you do not pass parameters, the default is used                                       ##
@@ -11,31 +11,14 @@
 red_color="\033[31m"
 green_color="\033[32m"
 text_end="\033[0m"
-
-
-# 定义默认值
-backup_file="/mysqlbackup/2023-07-03-15-00-33/2023-07-03-15-00-33-mydatabase.sql"
+start_time=$(date +%s)
+backup_file=""
 username="root"
 password="root"
-database_name="mydatabase"
-table_name="student"
+database_name=""
+table_name=""
+tip=""
 
-# 检查是否有参数传递，如果有则覆盖默认值
-while getopts "f:u:p:d:t:*" opt; do
-  case $opt in
-    f) backup_file="$OPTARG";;
-    u) username="$OPTARG";;
-    p) password="$OPTARG";;
-    d) database_name="$OPTARG";;
-    t) table_name="$OPTARG";;
-    *) echo "未知参数";;
-  esac
-done
-# 检查备份文件是否存在
-if [ ! -f "$backup_file" ]; then
-  echo "Backup file not found: $backup_file"
-  exit 1
-fi
 
 restore_single_table_data() {
 	# 还原单表数据
@@ -45,6 +28,7 @@ restore_single_table_data() {
 	docker cp $table_name.sql MySQL8:/$table_name.sql
 	docker exec MySQL8 /bin/bash -c "mysql -u $username -p$password -e 'use $database_name;' -e 'source /$table_name.sql;'"
 	echo -e "${green_color}-----------恢复完毕！-----------${text_end}"
+	echo -e "${green_color}-----本次是使用${backup_file}进行数据恢复-----------${text_end}"
 
 }
 
@@ -56,6 +40,7 @@ restore_the_entire_library_data() {
 	docker cp $backup_file MySQL8:/backup_file.sql
 	docker exec MySQL8 /bin/bash -c "mysql -u $username -p$password -e 'use $database_name;' -e 'source /backup_file.sql;'"
 	echo -e "${green_color}-----------恢复完毕！-----------${text_end}"
+	echo -e "${green_color}-----本次是使用${backup_file}进行数据恢复-----------${text_end}"
 }
 
 
@@ -64,7 +49,64 @@ Restore_a_database_in_full() {
 	echo -e "${red_color}----------本次进行全量中单库据恢复-----------${text_end}"
 	docker exec MySQL8 /bin/bash -c "mysql -u $username -p$password  --one-database $database_name" < $backup_file
 	echo -e "${green_color}----------恢复完毕！-----------${text_end}"
+	echo -e "${green_color}-----本次是使用${backup_file}进行数据恢复-----------${text_end}"
 }
 
-# 功能函数调用,从下行开始
-restore_the_entire_library_data;
+
+usage() {
+  echo "脚本使用方法:    bash $0 <选项> <参数>"
+  echo "选项说明:"
+  echo " -f <文件路径>:  数据保存路径,绝对路径 （默认为空）"
+  echo " -u <用户名>:   指定用户名 (默认为空)"
+  echo " -p <密码>:     指定密码 (默认为空)"
+  echo " -d <数据库名>:  指定数据库名 (默认为空)"
+  echo " -b <数据表>:  指定数据库表 (默认为空)"
+  echo " -t <恢复种类>:  指定恢复 数据 table(集合) 或 singlelibrary(数据库)  或 fulllibrary(全库) (默认为空)"
+  exit 1
+}
+
+
+# 检查是否有参数传递，如果有则覆盖默认值
+while getopts ":f:u:p:d:b:t:*:" opt; do
+  case $opt in
+    f) backup_file="$OPTARG";;
+    u) username="$OPTARG";;
+    p) password="$OPTARG";;
+    d) database_name="$OPTARG";;
+    b) table_name="$OPTARG";;
+    t) tip="$OPTARG";;
+    *) echo "参数传入有误: -$OPTARG"; usage;;
+  esac
+
+
+done
+# 检查备份文件是否存在
+if [ ! -f "$backup_file" ]; then
+  echo "Backup file not found: $backup_file"
+  exit 1
+fi
+
+
+case $tip in
+    table)
+      echo -e "${green_color}Mysql database_name: ${database_name}${text_end}"
+      echo -e "${green_color}Mysql database_name: ${collection_name}${text_end}"
+      restore_single_table_data;
+      ;;
+    singlelibrary)
+      echo -e "${green_color}Mysql database_name: ${database_name}${text_end}"
+      restore_the_entire_library_data;
+      ;;
+    fulllibrary)
+      echo -e "${green_color}Mysql database_name: ${database_name}${text_end}"
+      Restore_a_database_in_full;
+      ;;
+    *)
+      echo -e "${red_color}暂时无法确认你要恢复那些数据${text_end}"
+esac
+
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+
+echo -e "${green_color}本次执行脚本耗时为：$execution_time 秒${text_end}"
