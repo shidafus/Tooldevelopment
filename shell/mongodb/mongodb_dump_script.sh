9570196343c2a622116b45f9c80ed33f54d349d4
zjh <zjh13320020268@163.com>
2023-07-31 17:15:33 +0800

    新增shell脚本

--- shell/mongodb/mongodb_dump_script.sh
+++ shell/mongodb/mongodb_dump_script.sh
@@ -2,39 +2,69 @@
 #################################################################################################################
 ##   create_time    : 2023/6/30                                                                                ##
 ##   Author         : Ops-DaBao                                                                                ##
+##   Mail           : zjh13320020268@163.com                                                                   ##
 ##   Feature        : Backing up mongodb Data                                                                  ##
 ##   Usage          : When executing the script, you can pass parameters.                                      ##
 ##                    If you do not pass parameters, the default is used                                       ##
-##################################################################################################################
+#################################################################################################################
 # shellcheck disable=SC2034
 red_color="\033[31m"
 green_color="\033[32m"
 text_end="\033[0m"
-
-#保存备份个数，备份31天数据
-number=31
-#日期
+start_time=$(date +%s)
+numberdays="31"
 dd=`date +%Y-%m-%d-%H-%M-%S`
-#备份保存路径
-backup_dir=/mongobackup/$dd
-#用户名
-username="admin"
-#密码
-password="xqqwe123qwe123p0EW"
-#将要备份的数据库 ,一个数组
+backup_dir=/data/backup/mongodb
+username=""
+password=""
 database_name=""
-tip=""
+tip="all"
 
+usage() {
+  echo "脚本使用方法: bash $0 <选项> <参数>"
+  echo "选项说明:"
+  echo " -f <目录路径>: 数据保存路径 （默认为 /data/backup/mongodb）"
+  echo " -u <用户名>: 指定用户名 (默认为空)"
+  echo " -p <密码>: 指定密码 (默认为空)"
+  echo " -d <数据库名>: 指定数据库名 (默认为空)"
+  echo " -t <备份方式>: single  or  all (默认为all)  当为all时，指定的数据库参数无效"
+  echo " -n <天数>: 指定删除多少天前的无效数据 (默认为31天)"
+  exit 1
+}
 
+backup_single_collection() {
+	# 开始备份数据，单个数据库独立备份
+	echo -e "${red_color}----------本次进行单库备份-----------$text_end"
+	docker exec mongodb /bin/bash -c "mongodump -u $username -p$password --authenticationDatabase admin --archive --db $database_name" 1> $backup_dir/$database_name-$dd.archive 2> /tmp/$database_name-$dd.log;
+	echo -e "${green_color}-----------${DB}数据库备份完毕-----------$text_end"
+	echo -e "${green_color}-----------数据保存在 （$backup_dir/$database_name-$dd.archive） 中-----------$text_end"
+}
+
+full_backup() {
+	# 备份所有数据到同一份文件中
+	echo -e "${red_color}-----------本次进行全量备份-----------$text_end"
+	docker exec mongodb /bin/bash -c "mongodump -u $username -p$password  --authenticationDatabase admin --archive" 1> $backup_dir/all-$dd.archive 2> /tmp/all-$dd.log
+	echo -e "${green_color}----------备份完毕！-----------$text_end"
+	echo -e "${green_color}-----------数据保存在 （$backup_dir/all-$dd.archive） 中-----------$text_end"
+}
+
+spatial_optimization() {
+	#找出需要删除的备份
+	echo -e "${red_color}----------开始查询${numberdays}天以前的数据并删除-----------$text_end"
+	find $backup_dir/* -maxdepth 1 -type d -ctime +${numberdays} -exec rm -rf {} \;
+	echo -e "${green_color}-----------过期数据清理完毕！-----------$text_end"
+}
 
 # 检查是否有参数传递，如果有则覆盖默认值
-while getopts ":u:p:d:t:*" opt; do
+while getopts ":f:u:p:d:t:n:h:*:" opt; do
 	case $opt in
+	  f) backup_dir="$OPTARG";;
 	  u) username="$OPTARG";;
 	  p) password="$OPTARG";;
 	  d) database_name="$OPTARG";;
 	  t) tip="$OPTARG";;
-	  *) echo "未知参数";;
+	  n) numberdays="$OPTARG";;
+	  *) echo "参数传入有误: -$OPTARG"; usage;;
 	esac
 done
 
@@ -42,28 +72,6 @@ done
 if [ ! -d $backup_dir ]; then
 	  mkdir -p $backup_dir;
 fi
-
-
-backup_single_collection() {
-	# 开始备份数据，单个数据库独立备份
-	echo -e "${red_color}----------本次进行单库备份-----------$text_end"
-	docker exec mongodb /bin/bash -c "mongodump -u $username -p$password --authenticationDatabase admin --archive --db $database_name" 1> $backup_dir/$database_name.archive 2> /tmp/$dd-$database_name.log;
-	echo -e "${green_color}-----------${DB}数据库备份完毕-----------$text_end"
-}
-
-full_backup() {
-	# 备份所有数据到同一份文件中
-	echo -e "${red_color}-----------本次进行全量备份-----------$text_end"
-	docker exec mongodb /bin/bash -c "mongodump -u $username -p$password  --authenticationDatabase admin --archive" 1> $backup_dir/all.archive 2> /tmp/$dd-all.log
-	echo -e "${green_color}----------备份完毕！-----------$text_end"
-}
-
-spatial_optimization() {
-	#找出需要删除的备份
-	echo -e "${red_color}----------开始查询过期数据-----------$text_end"
-	find /mongobackup/* -maxdepth 1 -type d -ctime +31 -exec rm -rf {} \;
-	echo -e "${green_color}-----------过期数据清理完毕！-----------$text_end"
-}
 
 
 case $tip in
@@ -80,3 +88,8 @@ case $tip in
     *)
       echo -e "${red_color}暂时无法确认你要备份那些数据${text_end}"
 esac
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+
+echo -e "${green_color}本次执行脚本耗时为：$execution_time 秒${text_end}"
--- shell/mongodb/mongodb_resocer_script.sh
+++ shell/mongodb/mongodb_resocer_script.sh
@@ -2,7 +2,8 @@
 ##################################################################################################################
 ##   Create_time     : 2023/6/30                                                                                ##
 ##   Author          : Ops-DaBao                                                                                ##
-##   Feature         : restore mongodb Data                                                                       ##
+##   Mail            : zjh13320020268@163.com                                                                   ##
+##   Feature         : restore mongodb Data                                                                     ##
 ##   Usage           : When executing the script, you can pass parameters.                                      ##
 ##                     If you do not pass parameters, the default is used                                       ##
 ##################################################################################################################
@@ -10,8 +11,7 @@
 red_color="\033[31m"
 green_color="\033[32m"
 text_end="\033[0m"
-
-# 定义默认值
+start_time=$(date +%s)
 backup_file=""
 username=""
 password=""
@@ -19,30 +19,25 @@ database_name=""
 collection_name=""
 tip=""
 
-# 检查是否有参数传递，如果有则覆盖默认值
-while getopts "f:u:p:d:c:t:*" opt; do
-  case $opt in
-    f) backup_file="$OPTARG";;
-    u) username="$OPTARG";;
-    p) password="$OPTARG";;
-    d) database_name="$OPTARG";;
-    c) collection_name="$OPTARG";;
-    t) tip="$OPTARG";;
-    *) echo "未知参数";;
-  esac
-done
 
-# 检查备份文件是否存在
-if [ ! -f "$backup_file" ]; then
-  echo "Backup file not found: $backup_file"
+usage() {
+  echo "脚本使用方法:    bash $0 <选项> <参数>"
+  echo "选项说明:"
+  echo " -f <文件路径>:  数据保存路径,绝对路径 （默认为空）"
+  echo " -u <用户名>:   指定用户名 (默认为空)"
+  echo " -p <密码>:     指定密码 (默认为空)"
+  echo " -d <数据库名>:  指定数据库名 (默认为空)"
+  echo " -c <数据集合>:  指定数据库集合名 (默认为空)"
+  echo " -t <恢复种类>:  指定恢复 数据 assemble(集合) 或 singlelibrary(数据库)  或 fulllibrary(全库) (默认为空)"
   exit 1
-fi
+}
 
 restore_single_table_data() {
 	# 还原单个集合的数据
 	echo -e "${red_color}-----------本次进行单集合数据恢复-----------${text_end}"
 	docker exec mongodb /bin/bash -c "mongorestore -u $username -p$password --authenticationDatabase admin --archive  --db $database_name --collection  $collection_name" < $backup_file
 	echo -e "${green_color}-----------恢复完毕！-----------${text_end}"
+	echo -e "${green_color}-----本次是使用${backup_file}进行数据恢复-----------${text_end}"
 }
 
 restore_the_entire_library_data() {
@@ -50,6 +45,7 @@ restore_the_entire_library_data() {
 	echo -e "${red_color}-----------本次进行单库据恢复-----------${text_end}"
 	docker exec mongodb /bin/bash -c "mongorestore -u $username -p$password --authenticationDatabase admin --archive  --db $database_name" < $backup_file
 	echo -e "${green_color}-----------恢复完毕！-----------${text_end}"
+	echo -e "${green_color}-----本次是使用${backup_file}进行数据恢复-----------${text_end}"
 }
 
 
@@ -58,10 +54,28 @@ Restore_a_database_in_full() {
 	echo -e "${red_color}----------本次进行全量中单库据恢复-----------${text_end}"
 	docker exec MySQL8 /bin/bash -c "mongorestore -u $username -p$password  --authenticationDatabase admin --archive  --db $database_name" < $backup_file
 	echo -e "${green_color}----------恢复完毕！-----------${text_end}"
+	echo -e "${green_color}-----本次是使用${backup_file}进行数据恢复-----------${text_end}"
 }
 
-# 功能函数调用,从下行开始
-restore_the_entire_library_data;
+
+# 检查是否有参数传递，如果有则覆盖默认值
+while getopts ":f:u:p:d:c:t:*:" opt; do
+  case $opt in
+    f) backup_file="$OPTARG";;
+    u) username="$OPTARG";;
+    p) password="$OPTARG";;
+    d) database_name="$OPTARG";;
+    c) collection_name="$OPTARG";;
+    t) tip="$OPTARG";;
+    *) echo "参数传入有误: -$OPTARG"; usage;;
+  esac
+done
+
+# 检查备份文件是否存在
+if [ ! -f "$backup_file" ]; then
+  echo "Backup file not found: $backup_file"
+  exit 1
+fi
 
 
 case $tip in
@@ -81,3 +95,9 @@ case $tip in
     *)
       echo -e "${red_color}暂时无法确认你要恢复那些数据${text_end}"
 esac
+
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+
+echo -e "${green_color}本次执行脚本耗时为：$execution_time 秒${text_end}"
